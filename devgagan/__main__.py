a5a4fca9d836b902f5836b7f9282d3e3c1c7f6a3
poper12 <samyakdhengle89@gmail.com>
2025-01-31 19:44:48 +0530

    __main__.py

--- devgagan/__main__.py
+++ devgagan/__main__.py
@@ -2,14 +2,6 @@
 # File Name: __main__.py
 # Description: A Pyrogram bot for downloading files from Telegram channels or groups 
 #              and uploading them back to Telegram.
-# Author: Gagan
-# GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
-# YouTube: https://youtube.com/@dev_gagan
-# Created: 2025-01-11
-# Last Modified: 2025-01-11
-# Version: 2.0.5
-# License: MIT License
 # ---------------------------------------------------
 
 import asyncio
@@ -40,10 +32,9 @@ async def devggn_boot():
 📂 Bot Deployed successfully ...
 📝 Description: A Pyrogram bot for downloading files from Telegram channels or groups 
                 and uploading them back to Telegram.
-👨‍💻 Author: Gagan
-🌐 GitHub: https://github.com/devgaganin/
-📬 Telegram: https://t.me/team_spy_pro
-▶️ YouTube: https://youtube.com/@dev_gagan
+👨‍💻 Author: Aaru
+🌐 GitHub: https://github.com/Aaru2075/
+📬 Telegram: https://t.me/aaru_2074
 🗓️ Created: 2025-01-11
 🔄 Last Modified: 2025-01-11
 🛠️ Version: 2.0.5
